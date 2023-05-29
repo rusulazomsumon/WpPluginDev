26760bad15c098557eacda745ff61a06d6f1dde7
RusulAzomSumon <18103020@iubat.edu>
2023-05-29 23:56:59 +0600

    result-ms pluing created

---
+++ wp-content/plugins/result-ms/result-ms.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: Result Manegment System
+ * Plugin URI: https://rusulazom.me/plugin/result-ms
+ * Description: A plugin to manegment institude result.
+ * Version: 1.0
+ * Author: Rusul Azom
+ * Author URI: https://rusulazom.me
+**/
